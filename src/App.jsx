b6bbcfe1c41b4b22ff016a2b71e9b711644285e8
yusufyaman07 <yamanyusuf00000@gmail.com>
2024-11-12 14:53:52 +0300

    Creating and shaping the todo App interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Todo from "./components/todo";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-4xl bg-red-400">Yusuf YAMAN</h1>
+    <div className="bg-[#1A1A19] h-screen grid ">
+      <Todo />
     </div>
   );
 };
---
+++ src/components/todoItem/index.jsx
@@ -0,0 +1,14 @@
+import { FaRegCircle } from "react-icons/fa";
+import { RiDeleteBin5Fill } from "react-icons/ri";
+import { IoCheckmarkCircleSharp } from "react-icons/io5";
+const TodoItem = ({ text }) => {
+  return (
+    <div className="flex items-center gap-2 px-2 py-4 pb-4 border-b cursor-pointer">
+      <FaRegCircle className="text-[#C6E7FF] size-4" />
+      <p className="flex-1">{text}</p>
+      <RiDeleteBin5Fill className="text-[#CC2B52] size-4 hover:scale-110 transition-all" />
+    </div>
+  );
+};
+
+export default TodoItem;
